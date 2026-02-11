e8dca1dee9d9c605f7920fb81057135c223fce14
papsrowan <papsrowan@gmail.com>
2026-02-11 17:13:51 +0100

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Collecte Journalière - Administration",
   description: "Application de gestion de collectes journalières",
+    icons: {
+    icon: "/favicon.ico", // ou "/favicon.png"
+  },
 };
 
 export const viewport = {
